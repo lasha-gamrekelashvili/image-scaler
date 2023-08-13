6c105f5a9b53fde20ef4af1476c2be72524f91ac
lasha-gamrekelashvili <lash.gamrekelashvili@gmail.com>
2023-08-13 14:28:10 +0400

    remove redundant config properties

--- src/main/java/com/example/imagescaler/ImageScalerApplication.java
+++ src/main/java/com/example/imagescaler/ImageScalerApplication.java
@@ -1,14 +1,11 @@
 package com.example.imagescaler;
 
-import com.example.imagescaler.upscale.configuration.UpscaleApiConfig;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 @OpenAPIDefinition
-@EnableConfigurationProperties(UpscaleApiConfig.Configuration.class)
 public class ImageScalerApplication {
 
   public static void main(String[] args) {
